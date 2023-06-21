9c73c4057397a864c6c95ee2bed25f2a198d451b
beomq <beomdolq@gmail.com>
2023-06-21 11:18:45 +0900

    MODIFY: background color

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -11,7 +11,9 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.cast),
